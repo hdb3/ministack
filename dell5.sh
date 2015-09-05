0b455fe84420eb24ce7f9bc82aa748741400c0ca
Nicholas Hart <nic@lancs>
2015-09-05 15:16:33 +0100

    tidying

--- dell5.sh
+++ dell5.sh
@@ -1,4 +1,1 @@
-export OS_TENANT_NAME=admin
-export OS_USERNAME=admin
-export OS_PASSWORD=admin
-export OS_AUTH_URL=http://10.30.65.205:35357/v2.0
+export OS_TENANT_NAME=admin OS_USERNAME=admin OS_PASSWORD=admin OS_AUTH_URL=http://10.30.65.205:35357/v2.0
