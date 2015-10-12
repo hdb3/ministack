62aa1629abbb3d41b3982f8451777f66a6567876
Nic Hart <nic@lancs.uni>
2015-10-12 13:57:05 +0100

    switch1.py - config for VLAN code example

---
+++ switch1.py
@@ -0,0 +1,23 @@
+
+spec = {
+    'name' : "switch 1 VLANs",
+    'external network name' : "exnet3",
+    'keypair' : "X220",
+    'controller' : "r720",
+    'dns' : "10.30.65.200",
+    'credentials' : { 'user' : "nic", 'password' : "nic", 'project' : "nic" },
+    # 'credentials' : { 'user' : "admin", 'password' : "admin", 'project' : "admin" },
+    # NOTE: network start/end range -
+    #       a) must not include the gateway IP
+    #       b) when assigning host IPs remember taht a DHCP server will be allocated from the range as well as the hosts
+    #          Probably on the first or second available IP in the range....
+    'keypair' : "x220",
+    'Networks' : [
+        { 'name' : "net4001" , "start": "192.168.1.200", "end": "192.168.1.254", "subnet" :" 192.168.1.0/24", "gateway": "192.168.1.1", "vlan": 4001, "physical_network": "vlannet" },
+        { 'name' : "net4002" , "start": "192.168.1.200", "end": "192.168.1.254", "subnet" :" 192.168.1.0/24", "gateway": "192.168.1.1", "vlan": 4002, "physical_network": "vlannet" }
+    ],
+    'Hosts' : [
+        { 'name' : "h4001" , 'image' : "cirros" , 'flavor':"m1.tiny" , 'net' : [ ("net4001" , "192.168.1.201" , "*") ] },
+        { 'name' : "h4002" , 'image' : "cirros" , 'flavor':"m1.tiny" , 'net' : [ ("net4001" , "192.168.1.202" , "*") ] }
+    ]
+}
