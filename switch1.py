521c12f85f72d6e4882531b0ebf424f8c688fe2f
Nic Hart <nic@lancs.uni>
2015-10-12 16:22:38 +0100

    checking in the configs for the floodlight test

--- switch1.py
+++ switch1.py
@@ -13,11 +13,12 @@ spec = {
     #          Probably on the first or second available IP in the range....
     'keypair' : "x220",
     'Networks' : [
-        { 'name' : "net4001" , "start": "192.168.1.200", "end": "192.168.1.254", "subnet" :" 192.168.1.0/24", "gateway": "192.168.1.1", "vlan": 4001, "physical_network": "vlannet" },
-        { 'name' : "net4002" , "start": "192.168.1.200", "end": "192.168.1.254", "subnet" :" 192.168.1.0/24", "gateway": "192.168.1.1", "vlan": 4002, "physical_network": "vlannet" }
+        { 'name' : "net4001" , "start": "192.168.99.200", "end": "192.168.99.254", "subnet" :" 192.168.99.0/24", "gateway": "192.168.99.1", "vlan": 4001, "physical_network": "vlannet" },
+        { 'name' : "net4002" , "start": "192.168.99.200", "end": "192.168.99.254", "subnet" :" 192.168.99.0/24", "gateway": "192.168.99.1", "vlan": 4002, "physical_network": "vlannet" },
+        { 'name' : "net4000-mgmt" , "start": "192.168.1.200", "end": "192.168.1.254", "subnet" :" 192.168.1.0/24", "gateway": "192.168.1.1" }
     ],
     'Hosts' : [
-        { 'name' : "h4001" , 'image' : "cirros" , 'flavor':"m1.tiny" , 'net' : [ ("net4001" , "192.168.1.201" , "*") ] },
-        { 'name' : "h4002" , 'image' : "cirros" , 'flavor':"m1.tiny" , 'net' : [ ("net4001" , "192.168.1.202" , "*") ] }
+        { 'name' : "h4001" , 'image' : "cirros" , 'flavor':"m1.tiny" , 'net' : [ ("net4000-mgmt" , "*", "*") , ("net4001" , "192.168.99.201" ) ] },
+        { 'name' : "h4002" , 'image' : "cirros" , 'flavor':"m1.tiny" , 'net' : [ ("net4000-mgmt" , "*", "*") , ("net4002" , "192.168.99.202" ) ] }
     ]
 }
