777d2d3fecb7c4a4782a116c60e9c88bc3df782e
Nicholas Hart <nic@lancs>
2015-10-20 15:54:46 +0100

    fixup kilo.py to use DNS host names and actually work

--- kilo.py
+++ kilo.py
@@ -12,19 +12,19 @@ spec = {
     #       b) when assigning host IPs remember taht a DHCP server will be allocated from the range as well as the hosts
     #          Probably on the first or second available IP in the range....
     'Networks' : [
-        { 'name' : "odldemo" , "start": "192.168.50.2", "end": "192.168.50.254", "subnet" :" 192.168.50.0/24", "gateway": "192.168.50.1" },
-        { 'name' : "odldemo2" , "start": "192.168.111.2", "end": "192.168.111.254", "subnet" :" 192.168.111.0/24", "gateway": "192.168.111.1" },
-        { 'name' : "odldemo3" , "start": "172.16.0.2", "end": "172.16.0.254", "subnet" :" 172.16.0.0/24", "gateway": "172.16.0.1" },
+        { 'name' : "kilo" , "start": "192.168.0.2", "end": "192.168.0.254", "subnet" :" 192.168.0.0/24", "gateway": "192.168.0.1" },
+        { 'name' : "kilo-dataplane" , "start": "172.16.0.2", "end": "172.16.0.254", "subnet" :" 172.16.0.0/24", "gateway": "172.16.0.1" },
+        { 'name' : "kilo-provider" , "start": "172.16.1.2", "end": "172.16.1.254", "subnet" :" 172.16.1.0/24", "gateway": "172.16.1.1" },
     ],
     # Hint: list explicity required external IPs first to avoid them being claimed by hosts that don't care...
     'Hosts' : [
-        { 'name' : "devstack-control" , 'image' : "trusty64" , 'flavor':"m1.xlarge" , 'net' : [ ("odldemo" , "192.168.50.20", "10.30.65.80"),
-                                                                                                ("odldemo2" , "192.168.111.10", None),
-                                                                                                ("odldemo3" , "172.16.0.10", None) ] },
-        { 'name' : "devstack-compute-1" , 'image' : "trusty64" , 'flavor':"m1.xlarge" , 'net' : [ ("odldemo" , "192.168.50.21","10.30.65.81"),
-                                                                                                  ("odldemo2" , "192.168.111.11", None),
-                                                                                                  ("odldemo3" , "172.16.0.11", None) ] },
-        # { 'name' : "devstack-compute-2" , 'image' : "trusty64" , 'flavor':"m1.xlarge" , 'net' : [ ("odldemo" , "192.168.50.22", None) ] },
-        # { 'name' : "devstack-compute-3" , 'image' : "trusty64" , 'flavor':"m1.xlarge" , 'net' : [ ("odldemo" , "192.168.50.23", None) ] },
+        { 'name' : "kilo-control" , 'image' : "Centos7" , 'flavor':"m1.xlarge" , 'net' : [
+                                                                                          ("kilo" , "192.168.0.20", "kilo-controller"),
+                                                                                         ] },
+        { 'name' : "kilo-compute-1" , 'image' : "Centos7" , 'flavor':"m1.xlarge" , 'net' : [
+                                                                                            ("kilo" , "192.168.0.21","kilo-compute"),
+                                                                                            ("kilo-dataplane" , "" ),
+                                                                                            ("kilo-provider" , "") ] },
+        { 'name' : "kilo-network-1" , 'image' : "Centos7" , 'flavor':"m1.xlarge" , 'net' : [ ("kilo" , "192.168.0.22","kilo-network"), ("kilo-dataplane" , ""), ("kilo-provider" , "") ] },
     ]
 }
