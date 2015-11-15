35c862a27e4b9ef310d12d1713c63c287d814721
Nicholas Hart <nic@lancs>
2015-11-15 20:15:28 +0000

    changes to topo files only

--- kilo-simple.py
+++ kilo-simple.py
@@ -1,11 +1,15 @@
 
 spec = {
-    'name' : "a 4-node kilo cluster",
+    'name' : "a simple kilo cluster",
     'external network name' : "exnet3",
     'keypair' : "X220",
     'controller' : "r720",
     'dns' : "10.30.65.200",
     'credentials' : { 'user' : "nic", 'password' : "nic", 'project' : "nic" },
+    # NOTE: network start/end range -
+    #       a) must not include the gateway IP
+    #       b) when assigning host IPs remember taht a DHCP server will be allocated from the range as well as the hosts
+    #          Probably on the first or second available IP in the range....
     'Networks' : [
         { 'name' : "kilo" , "start": "192.168.0.2", "end": "192.168.0.254", "subnet" :" 192.168.0.0/24", "gateway": "192.168.0.1" },
         { 'name' : "kilo-dataplane" , "subnet" :" 172.16.0.0/24" },
@@ -13,9 +17,8 @@ spec = {
     ],
     # Hint: list explicity required external IPs first to avoid them being claimed by hosts that don't care...
     'Hosts' : [
-        { 'name' : "kilo-controller" , 'image' : "Centos7" , 'flavor':"m1.xlarge" , 'net' : [ ("kilo" , "192.168.0.20", "kilo-controller"), ], 'script' : "test.sh" },
+        { 'name' : "kilo-controller" , 'image' : "Centos7" , 'flavor':"m1.xlarge" , 'net' : [ ("kilo" , "192.168.0.20", "kilo-controller"), ] },
+        { 'name' : "kilo-compute" , 'image' : "Centos7" , 'flavor':"m1.xlarge" , 'net' : [ ("kilo" , "192.168.0.21","kilo-compute"), ("kilo-dataplane"), ("kilo-provider") ] },
         { 'name' : "kilo-network" , 'image' : "Centos7" , 'flavor':"m1.xlarge" , 'net' : [ ("kilo" , "192.168.0.22","kilo-network"), ("kilo-dataplane"), ("kilo-provider") ] },
-        { 'name' : "kilo-compute-1" , 'image' : "Centos7" , 'flavor':"m1.xlarge" , 'net' : [ ("kilo" , "192.168.0.101","kilo-compute-1"), ("kilo-dataplane"), ("kilo-provider") ] },
-        { 'name' : "kilo-compute-2" , 'image' : "Centos7" , 'flavor':"m1.xlarge" , 'net' : [ ("kilo" , "192.168.0.102","kilo-compute-2"), ("kilo-dataplane"), ("kilo-provider") ] },
     ]
 }
---
+++ kilo-standalone.py
@@ -0,0 +1,16 @@
+
+spec = {
+    'name' : "a stand alone kilo instance",
+    'external network name' : "exnet3",
+    'keypair' : "X220",
+    'controller' : "r720",
+    'dns' : "10.30.65.200",
+    'credentials' : { 'user' : "nic", 'password' : "nic", 'project' : "nic" },
+    'Networks' : [
+        { 'name' : "kilo-standalone" , "start": "192.168.0.2", "end": "192.168.0.254", "subnet" :" 192.168.0.0/24", "gateway": "192.168.0.1" },
+        { 'name' : "kilo-standalone-provider" , "subnet" :" 172.16.1.0/24" },
+    ],
+    'Hosts' : [
+        { 'name' : "kilo-standalone" , 'image' : "Centos7" , 'flavor':"m1.xlarge" , 'net' : [ ("kilo-standalone" , "192.168.0.101","kilo-standalone"), ("kilo-standalone-provider") ] },
+    ]
+}
