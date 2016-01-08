12784442942c91adb3eb64e4efd4911e2eab0b77
Nic Hart <nic@nic-ThinkPad-X61s>
2016-01-08 15:10:16 +0000

    add atriuma.py

---
+++ atriuma.py
@@ -0,0 +1,20 @@
+
+spec = {
+    'name' : "a simple kilo cluster",
+    'external network name' : "exnet3",
+    'keypair' : "x61",
+    'controller' : "r720",
+    'dns' : "10.30.65.200",
+    'credentials' : { 'user' : "nic", 'password' : "nic", 'project' : "nic" },
+    # NOTE: network start/end range -
+    #       a) must not include the gateway IP
+    #       b) when assigning host IPs remember taht a DHCP server will be allocated from the range as well as the hosts
+    #          Probably on the first or second available IP in the range....
+    'Networks' : [
+        { 'name' : "atriuma" , "start": "192.168.0.2", "end": "192.168.0.254", "subnet" :" 192.168.0.0/24", "gateway": "192.168.0.1" },
+    ],
+    # Hint: list explicity required external IPs first to avoid them being claimed by hosts that don't care...
+    'Hosts' : [
+        { 'name' : "atriuma" , 'image' : "atriuma" , 'flavor':"m1.large" , 'net' : [ ("atriuma" , "192.168.0.20", "atriuma"), ] },
+    ]
+}
