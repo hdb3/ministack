df084d5b05214a46fd2c5f42b5b599450b73d751
nic <nic@dell4>
2016-02-02 10:05:19 +0000

    key change only

--- kilo-standalone.py
+++ kilo-standalone.py
@@ -2,7 +2,7 @@
 spec = {
     'name' : "a stand alone kilo instance",
     'external network name' : "exnet3",
-    'keypair' : "X61",
+    'keypair' : "openstack_rsa",
     'controller' : "r720",
     'dns' : "10.30.65.200",
     'credentials' : { 'user' : "nic", 'password' : "nic", 'project' : "nic" },
